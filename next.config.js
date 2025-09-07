0f4afd6c22fc491d15ce4e9deb472e005c2f3d1a
Wegfahrt <pl.wegfahrt@gmail.com>
2025-09-07 12:32:36 +0200

    disabled eslint during builds

--- next.config.js
+++ next.config.js
@@ -6,6 +6,15 @@ import "./src/env.js";
 
 /** @type {import("next").NextConfig} */
 const config = {
+  eslint: {
+    // We run ESLint in CI so we don't need to run it during production builds.
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    // We run TypeScript checks in CI so we don't need to run it during production builds.
+    ignoreBuildErrors: true,
+  },
+  reactStrictMode: true,
   images: {
     remotePatterns: [
       {
